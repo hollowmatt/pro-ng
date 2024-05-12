43b979a8de09ffae9752d97a1006a4ddc7cf95dc
Matt Holloway <hollowmatt@gmail.com>
2024-05-12 16:13:19 -0400

    add material

--- ch2/todo/src/app/app.component.ts
+++ ch2/todo/src/app/app.component.ts
@@ -1,12 +1,28 @@
+//Angular Components
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//Material Components
+import { FormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule  } from '@angular/material/table';
+import { MatCheckboxModule  } from '@angular/material/checkbox';
+import { MatFormFieldModule   } from '@angular/material/form-field';
+import { MatInputModule   } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+//Local Components
 import { TodoItem } from './todoItem';
 import { TodoList } from './todoList';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule,
+    MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule,
+    MatTableModule, MatCheckboxModule, MatFormFieldModule, MatInputModule,
+    MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
