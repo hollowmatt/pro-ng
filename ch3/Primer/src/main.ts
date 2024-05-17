abc6abe7e58589ec72697f8cd188c55785fabd0a
Matt Holloway <hollowmatt@gmail.com>
2024-05-16 21:04:25 -0400

    spread

--- ch3/Primer/src/main.ts
+++ ch3/Primer/src/main.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-function writeValue(val: string = "default value") {
-  console.log(`Value: ${val ?? "Falback value"}`)
+function writeValue(val: string = "default value", ...extraInfo: string []) {
+  console.log(`Value: ${val ?? "Fallback value"}, Extras: ${extraInfo}`)
 }
 
-writeValue("London");
+writeValue("London", "raining", "cold");
+writeValue("Paris");
+writeValue("Pittsburgh", "armpit");
 writeValue();
