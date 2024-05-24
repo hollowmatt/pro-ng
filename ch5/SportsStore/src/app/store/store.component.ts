3b564d9ef256a22f90e9c2178d15972bd2b1ac53
Matt Holloway <hollowmatt@gmail.com>
2024-05-24 09:16:01 -0400

    add category filter and setter on component

--- ch5/SportsStore/src/app/store/store.component.ts
+++ ch5/SportsStore/src/app/store/store.component.ts
@@ -1,4 +1,4 @@
-import { Component, Signal } from '@angular/core';
+import { Component, signal, computed, Signal } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductRepository } from '../model/product.repository';
 import { StaticDataSource } from '../model/static.datasource';
@@ -15,9 +15,21 @@ import { CommonModule } from '@angular/common';
 export class StoreComponent {
   products: Signal<Product[]>;
   categories: Signal<String[]>;
+  selectedCategory = signal<string | undefined>(undefined);
 
   constructor(private repository: ProductRepository) {
-    this.products = repository.products;
+    this.products = computed(() => {
+      if(this.selectedCategory() == undefined) {
+        return this.repository.products();
+      } else {
+        return this.repository.products().filter(p => 
+          p.category === this.selectedCategory());
+      }
+    })
     this.categories = repository.categories;
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory.set(newCategory);
   }
 }
