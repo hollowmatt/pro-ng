484d1fb91152b0835067c3e723fd6493c1d24109
Matt Holloway <hollowmatt@gmail.com>
2024-05-24 09:17:59 -0400

    add a few more products and additional category

--- ch5/SportsStore/src/app/model/static.datasource.ts
+++ ch5/SportsStore/src/app/model/static.datasource.ts
@@ -8,6 +8,10 @@ export class StaticDataSource {
         "Product 1 (Category 1)", 100),
     new Product(2, "Product 2", "Category 1", 
         "Product 2 (Category 1)", 100),
+    new Product(3, "Product 3", "Category 2", 
+        "Product 3 (Category 2)", 200),
+    new Product(4, "Product 4", "Category 2", 
+        "Product 4 (Category 2)", 200),
   ];
 
   products: Signal<Product[]> = signal<Product[]>(this.data)
