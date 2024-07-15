0b225e18f187d6388f199e82d129b768166b498f
Matt Holloway <hollowmatt@gmail.com>
2024-07-15 19:45:24 -0400

    back to housing

--- redo/todo/src/app/home/home.component.ts
+++ redo/todo/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
