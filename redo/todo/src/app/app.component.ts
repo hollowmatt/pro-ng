9b170f1521bb2b6b6da7d08ad558b171446e3f10
Matt Holloway <hollowmatt@gmail.com>
2024-07-17 20:18:42 -0400

    update view

--- redo/todo/src/app/app.component.ts
+++ redo/todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todoItem';
+import { TodoList } from './todoList';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  private list = new TodoList("Catnip", [
+    new TodoItem("Lick butt", true),
+    new TodoItem("Kiss mom", true),
+    new TodoItem("get treats"),
+    new TodoItem("nap"),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
 }
