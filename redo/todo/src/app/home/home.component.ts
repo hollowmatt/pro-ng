5fd671fcfd8f949a3851fbf70e20710dc055f2a1
Matt Holloway <hollowmatt@gmail.com>
2024-07-20 13:10:55 -0400

    render dynamic content

--- redo/todo/src/app/home/home.component.ts
+++ redo/todo/src/app/home/home.component.ts
@@ -11,14 +11,14 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  readonly baseUrl = `https://angular.io/assets/images/tutorials/faa`;
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
   housingLocation: HousingLocation = {
     id: 9999,
     name: 'My Home',
     city: 'Pittsburgh',
     state: 'PA',
-    photo: '${this.baseUrl}/example-house.jpg',
+    photo: `${this.baseUrl}/example-house.jpg`,
     availableUnits: 99,
     wifi: true,
     laundry: false,
