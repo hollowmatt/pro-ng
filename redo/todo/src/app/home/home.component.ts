f388ea616e0b60b2cf358910c6906f37a6de0af0
Matt Holloway <hollowmatt@gmail.com>
2024-07-20 12:59:52 -0400

    add housing location interface

--- redo/todo/src/app/home/home.component.ts
+++ redo/todo/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,5 +11,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = `https://angular.io/assets/images/tutorials/faa`;
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'My Home',
+    city: 'Pittsburgh',
+    state: 'PA',
+    photo: '${this.baseUrl}/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
