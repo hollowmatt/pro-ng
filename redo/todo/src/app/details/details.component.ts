6227fd3be354f0a4e172a8ad74a311c7769743c1
Matt Holloway <hollowmatt@gmail.com>
2024-07-23 20:14:00 -0400

    plug id param from route into page

--- redo/todo/src/app/details/details.component.ts
+++ redo/todo/src/app/details/details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-details',
@@ -8,5 +12,10 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
 
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
