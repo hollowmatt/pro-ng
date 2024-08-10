7c4b2863ac55357f275f70ae208658a97fe4c8de
Matt Holloway <hollowmatt@gmail.com>
2024-08-09 20:23:30 -0400

    custom directive for page numbers

--- redo/SportsStore/src/app/store/store.component.ts
+++ redo/SportsStore/src/app/store/store.component.ts
@@ -14,7 +14,8 @@ export class StoreComponent {
   productsPerPage = signal(4);
   selectedPage = signal(1);
   pagedProducts: Signal<Product[]>;
-  pageNumbers: Signal<number[]>;
+  // pageNumbers: Signal<number[]>;
+  pageCount: Signal<number>;
 
   constructor(private repository: ProductRepository) {
     this.categories = repository.categories;
@@ -35,8 +36,12 @@ export class StoreComponent {
       return this.products().slice(pageIndex(), pageIndex() + this.productsPerPage());
     })
 
-    this.pageNumbers = computed(() => {
-      return Array(Math.ceil(this.products().length / this.productsPerPage())).fill(0).map((x, i) => i + 1);
+    // this.pageNumbers = computed(() => {
+    //   return Array(Math.ceil(this.products().length / this.productsPerPage())).fill(0).map((x, i) => i + 1);
+    // })
+
+    this.pageCount = computed(() => {
+      return Math.ceil(this.products().length / this.productsPerPage());
     })
   }
 
