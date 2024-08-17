280b8c0a0e66fba3e6405004915274a185d4563a
Matt Holloway <hollowmatt@gmail.com>
2024-08-17 15:08:20 -0400

    add in routing for cart and checkout, with stubbed pages

--- redo/SportsStore_16/src/app/app.module.ts
+++ redo/SportsStore_16/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { StoreModule } from "./store/store.module";
+import { StoreComponent } from './store/store.component';
+import { CheckoutComponent } from './store/checkout.component';
+import { CartDetailComponent } from './store/cartDetail.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule],
+  imports: [BrowserModule, StoreModule,
+    RouterModule.forRoot([
+      { path: "store", component: StoreComponent },
+      { path: "cart", component: CartDetailComponent },
+      { path: "checkout", component: CheckoutComponent },
+      { path: "**", redirectTo: "/store" }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ redo/SportsStore_16/src/app/store/checkout.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: "checkout.component.html",
+  selector: "checkout"
+})
+export class CheckoutComponent {}
