d5df0c6d80e7304ccb3ad881d73199b5a3cbfacd
Matt Holloway <hollowmatt@google.com>
2024-08-17 00:18:59 +0000

    cart not properly working...

--- redo/SportsStore/src/app/store/cartSummary.component.ts
+++ redo/SportsStore/src/app/store/cartSummary.component.ts
@@ -3,7 +3,8 @@ import { Cart } from "../model/cart.model";
 
 @Component({
   selector: "cart-summary",
-  templateUrl: "cartSummary.component.html"
+  templateUrl: "cartSummary.component.html",
+  providers: [Cart]
 })
 export class CartSummaryComponent {
   constructor(public cart: Cart){}
